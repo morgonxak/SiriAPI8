e310687caa88988644d5cdd2a1afbb49c4cb12bd
Dima <morgon08@mail.ru>
2015-09-22 22:28:34 +0700

    Update search.py

--- SiriAPI8/search.py
+++ SiriAPI8/search.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import base64, re
-find_rus = re.compile(r'[а-яА-Я]')
+import base64
 class search:
     def __init__ (self, SiriAPI):
         self.SiriAPI = SiriAPI
